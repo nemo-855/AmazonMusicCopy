0605bb879d234dada5013003331f48cbea174718
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-07-17 16:18:27 +0900

    feat: Text のフォーマットを作成

---
+++ amazon_music_copy/ui/theme/TextAppearances.swift
@@ -0,0 +1,47 @@
+//
+//  TextAppearances.swift
+//  amazon_music_copy
+//
+//  Created by Kohei Inoue on 2022/07/17.
+//
+
+import Foundation
+import SwiftUI
+
+struct MediumText : View {
+    private let text: String
+    private let color: Color
+    
+    init(
+        text: String,
+        color: Color? = nil
+    ) {
+        self.text = text
+        self.color = color ?? Color(Colors.white.name)
+    }
+    
+    var body: some View {
+        Text(text)
+            .font(.system(size: 20))
+            .foregroundColor(color)
+    }
+}
+
+struct SmallText : View {
+    private let text: String
+    private let color: Color
+    
+    init(
+        text: String,
+        color: Color? = nil
+    ) {
+        self.text = text
+        self.color = color ?? Color(Colors.white.name)
+    }
+    
+    var body: some View {
+        Text(text)
+            .font(.system(size: 16))
+            .foregroundColor(color)
+    }
+}
