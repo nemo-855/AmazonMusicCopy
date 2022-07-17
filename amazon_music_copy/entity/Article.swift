fb0b1756edc1adc019055f317e20bd8637a1ecb8
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-07-17 19:46:19 +0900

    feat: entity 作成

---
+++ amazon_music_copy/entity/Article.swift
@@ -0,0 +1,16 @@
+//
+//  Article.swift
+//  amazon_music_copy
+//
+//  Created by Kohei Inoue on 2022/07/17.
+//
+
+import Foundation
+
+struct Article {
+    let id: String
+    let url: String
+    let title: String
+    let user: User
+}
+
---
+++ amazon_music_copy/entity/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  amazon_music_copy
+//
+//  Created by Kohei Inoue on 2022/07/17.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let description: String
+}
